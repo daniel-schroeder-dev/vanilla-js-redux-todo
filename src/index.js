ba2eb6c546b0311142d0d924fcf906382571bdc4
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-05-29 09:59:22 -0500

    Add event listener for form submit and setup dispatch of ADD_TODO.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import { createStore } from 'redux';
 import TodoItem from './components/TodoItem/TodoItem.js';
 
-const rootReducer = (state = [], action) => {
+const todoListContainer = document.getElementById('todoList');
+const form = document.querySelector('form');
+const createTodoInput = document.getElementById('createTodo');
+
+const store = createStore(
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	store.dispatch({ type: 'ADD_TODO', description: createTodoInput.value });
+});
+
+function rootReducer(state = [], action) {
 	return state;
 }
-
-const store = createStore(rootReducer);
-
-console.log(store)
-
-const todoListContainer = document.getElementById('todoList');
-const todoItem = new TodoItem('foo', todoListContainer);
-
-todoItem.render();
